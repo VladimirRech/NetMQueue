489d23ed4c704a03b701241afa6c51689a23cc52
Vladimir Rech <rechvladimir@hotmail.com>
2019-07-15 22:03:07 -0300

    Adding first endpoints and core functionality.

---
+++ Source/ClassicDotNet/NetMQueue.Classic.Api/Controllers/NetQueueController.cs
@@ -0,0 +1,23 @@
+﻿using NetMQueue.Classic.Queue;
+using NetMQueue.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetMQueue.Classic.Api.Controllers
+{
+    public class NetQueueController : ApiController
+    {
+        [HttpPost]
+        [Route("enqueue")]
+        public string Enqueue(MSolicitation mSolicitation)
+        {
+            BaseQueue baseQueue = new BaseQueue(".\\QueueTest");
+            baseQueue.Enqueue(mSolicitation);
+            return "OK";
+        }
+    }
+}
---
+++ Source/ClassicDotNet/NetMQueue.Classic.Queue/BaseQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMQueue.Classic.Queue
+{
+    // Getting started documentation: https://blog.jsinh.in/introduction-to-microsoft-message-queue-msmq/#.XS0gvxXPzDc
+    public class BaseQueue
+    {
+        string _queueName;
+        object _queueObject;
+
+        public BaseQueue(string queueName)
+        {
+            _queueName = queueName;
+        }
+
+        public void Enqueue(object queueObject)
+        {
+            _queueObject = queueObject;
+            MessageQueue mqueue = new MessageQueue(_queueName);
+            mqueue.Send(_queueObject);
+        }
+
+        public object Dequeue()
+        {
+            return _queueObject;
+        }
+    }
+}
